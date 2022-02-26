45e8781f9f9f79a754e4a59e00170af64db6509e
Gincysk <gincysk@gmail.com>
2022-02-26 09:39:23 -0500

    Added four assignments

--- src/com/company/ArraySum.java
+++ src/com/company/ArraySum.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class ArraySum {
     public static void main(String[] args) {
+
+        //To print sum of marks in array
 //        float marks[] = new float[]{3,4,6,5,8};
 //
 //
@@ -31,6 +33,8 @@ public class ArraySum {
 //        System.out.println("The sum of the numbers is:" +sum);
 
 
+
+        //To print first name using char array
 //        char name[] = new char[]{'G','i','n','c','y'};
 //
 //                    for(int i=0;i<5;i++) {
@@ -38,22 +42,66 @@ public class ArraySum {
 //                    }
 
 
+
+    // To print average of a column in array
+//        int arr[][] = new int[][]{
+//            {3, 6, 3},
+//            {5, 7, 5},
+//            {2, 9, 5},
+//            {1, 7, 4},
+//            {7, 3, 3}
+//        };
+//
+//        for (int i = 0; i < 3; i++) {
+//            float sum = 0;
+//            for ( int j = 0; j < 5; j++) {
+//                sum = sum + arr[j][i];
+//
+//            }
+//            float average = sum/arr.length;
+//            System.out.println("Average of column " +(i+1) + " is:  " + average);
+//        }
+
+
+
+
+
+        //To print boundaries of array
+
         int arr[][] = new int[][]{
-            {3, 6, 3},
-            {5, 7, 5},
-            {2, 9, 5},
-            {1, 7, 4},
-            {7, 3, 3}
+                {3, 6, 3, 6},
+                {5, 7, 1, 7},
+                {2, 9, 5, 2},
+                {1, 7, 4, 1},
         };
 
-        for (int i = 0; i < 3; i++) {
-            float sum = 0;
-            for ( int j = 0; j < 5; j++) {
-                sum = sum + arr[j][i];
+        System.out.println("The original 2D array is:");
+        for (int i = 0; i < 4; i++)
+        {
+            for ( int j = 0; j < 4; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("The array boundary is:");
+        for (int i = 0; i < 4 ; i++)
+        {
+            for ( int j = 0; j < 4 ; j++) {
+
+                if (i == 0 || j == 0 || i == arr.length -1 || j == arr.length -1)
+                {
+                    System.out.print(arr[i][j] +" ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
 
             }
-            float average = sum/arr.length;
-            System.out.println("Average of column " +(i+1) + " is:  " + average);
+            System.out.println("");
         }
+
     }
 }
