cef36468fd4df2d0f39211377468e327c0f4b378
Gincysk <gincysk@gmail.com>
2022-08-03 12:41:45 -0400

    Adding calculator class

---
+++ src/com/company/BasicCalculator.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class BasicCalculator {
+
+    public static void main(String[] args)
+
+        System.out.println(sum(6,4));
+        System.out.println(difference(7,3));
+        System.out.println(multiply(3,7));
+        System.out.println(divide(6,2));
+    }
+
+
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+
+    public static int difference(int a, int b) {
+        return a-b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public static int divide(int a, int b) {
+        return a/b;
+    }
+
+}
